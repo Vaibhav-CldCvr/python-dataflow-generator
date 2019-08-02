697e38a9f80190158920a4ca0fa34d7ff1b965fe
Vaibhav Kapur <cloudcover-vaibhav@Vaibhavs-MacBook-Air.local>
2019-08-02 12:49:53 +0530

    added configuration file for dataflow

--- main.py
+++ main.py
@@ -1,6 +1,27 @@
+import json
+
+
+def generate_dataflow_configuration(*args):
+    if args[2] == 'stream':
+        streaming = True
+    else:
+        streaming = False
+
+    config = {
+        "project": args[0],
+        "temp_location": args[1],
+        "streaming": streaming,
+        "runner": args[3],
+        "read_gsc_location": args[4],
+        "bq_dataset": args[5],
+        "bq_table": args[6],
+        "setup_file": args[0]+'/setup.py'
+    }
+    return config
 
 
 def run():
+    flag = False
     project_folder_name = input('Enter your project folder name: ')
     if project_folder_name:
         df_type = input(
@@ -9,6 +30,7 @@ def run():
         if df_type == 'stream':
             # set df streaming parameter
             pass
+        df_type = 'batch'
 
         gcp_project_id = input('Enter your gcp project id: ')
         if gcp_project_id:
@@ -16,6 +38,7 @@ def run():
             if gsc_temp_location:
                 df_runner = input(
                     'Enter dataflow runner type: DataflowRunner/DirectRunner: ')
+
                 type_of_df = input("Default to 1. GCS to BiqQuery: ")
                 type_of_df = '1'
                 if type_of_df == '1':
@@ -25,6 +48,14 @@ def run():
                         'Enter big query dataset name: ')
                     bq_table = input(
                         'Enter big query table name: ')
+                    flag = True
+
+    if flag == True:
+        config = generate_dataflow_configuration(
+            gcp_project_id, gsc_temp_location, df_type, df_runner, read_gsc_location, bq_dataset, bq_table)
+
+        with open('/Users/cloudcover-vaibhav/python-dataflow-generator/app_code/config/mapping.json', 'w') as p:
+            json.dump(config, p)
 
 
 if __name__ == "__main__":
