4f40088444042f3b6407cc5e01061ebe99862a59
Vaibhav Kapur <cloudcover-vaibhav@Vaibhavs-MacBook-Air.local>
2019-08-02 11:52:21 +0530

    added scaffolding for df and input parametes.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 
+
 def run():
     project_folder_ name = input('Enter your project folder name: ')
     if project_folder_:
@@ -7,16 +8,23 @@ def run():
 
         if df_type == 'stream':
             # set df streaming parameter
-            gcp_project_id = input('Enter your gcp project if: ')
-            if gcp_project_id:
-                gsc_temp_location = input('Enter temp path: ')
-                if gsc_temp_location:
-                    df_runner = input(
-                        'Enter dataflow runner type: DataflowRunner/DirectRunner')
-                    if df_runner = 'DataflowRunner':
-                        # create venv
-                        # create requirments.txt
-                        # create setup.py
+            pass
+
+        gcp_project_id = input('Enter your gcp project id: ')
+        if gcp_project_id:
+            gsc_temp_location = input('Enter gcs temp path location: ')
+            if gsc_temp_location:
+                df_runner = input(
+                    'Enter dataflow runner type: DataflowRunner/DirectRunner')
+                if df_runner:
+                    type_of_df = input("Default to 1. GCS to BiqQuery")
+                    if type_of_df == '1':
+                        read_gsc_location = input(
+                            'Enter gcs location path: ')
+                        bq_dataset = input(
+                            'Enter big query dataset name: ')
+                        bq_table = input(
+                            'Enter big query table name: ')
 
 
 if __name__ == "__main__":
